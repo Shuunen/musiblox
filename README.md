2889a191163d61d032a82095971bf2064db668b0
Romain Racamier-Lafon <romain.racamier@gmail.com>
2021-04-24 03:45:35 +0200

    working poc

--- README.md
+++ README.md
@@ -6,6 +6,12 @@
 
 ## Thanks
 
+- [Animate.css](https://github.com/animate-css/animate.css/blob/main/source/attention_seekers/heartBeat.css) : for the heart beat animation
+- [Favicon.io](https://favicon.io/favicon-generator/) : for the favicon
+- [Gifer](https://gifer.com/) : for the win/loose ending gifs
 - [Github](https://github.com) : for all their great work year after year, pushing OSS forward
+- [Joshua Empyre](https://freesound.org/people/joshuaempyre/sounds/250856/) : for music loop
+- [Kimberly Geswein](https://kimberlygeswein.com) : for the favicon font Gloria Hallelujah
+- [Paomedia](https://www.iconfinder.com/icons/285652/disc_vinyl_icon) : nice disc icon
 - [Shields.io](https://shields.io) : for the nice badges on top of this readme
 - [Xo](https://github.com/xojs/xo) : super tool to find & fix problems
--- public/main.js
+++ public/main.js
@@ -1,29 +1,89 @@
 /* global document, window */
 
-class Game {
-	constructor() {
-		this.setElements();
-		this.setBoard();
-	}
+const rand = (min = 0, max = 100) => Math.floor((Math.random() * (max - min + 1)) + min)
+const pick = items => items[Math.floor(Math.random() * items.length)]
 
-	setElements() {
-		this.board = document.querySelector('.board');
-	}
+const WIN_GIFS = ['4A5', '7FZs']
+const LOOSE_GIFS = ['W4S', '1A8H', '6IJz']
+const MAX_DELAY = 600
 
-	setBoard() {
-		let html = '';
-		const hue = 270;
-		const gap = 10;
-		for (let i = 0; i < 25; i++) {
-			html += `<span class=brick style="background-color: hsl(${hue - (i * gap)}deg 50% 40%)"></span>`;
-		}
+class Brick {
+  constructor() {
+    const element = document.createElement('span')
+    element.classList.add('brick')
+    this.element = element
+    this.active = false
+    this.changeColor()
+  }
 
-		this.board.innerHTML = html;
-	}
+  changeColor() {
+    this.element.style.backgroundColor = `hsl(${rand(0, 12) * 30}deg 80% 60%)`
+    if (this.active) setTimeout(() => this.changeColor(), rand(200, MAX_DELAY))
+  }
 
-	randomColor() {
-		return Math.floor((Math.random() * 2) ** 24).toString(16).padStart(6, '0');
-	}
+  toggleActive() {
+    this.active = !this.active
+    if (this.active) this.changeColor()
+  }
 }
 
-window.addEventListener('load', () => new Game());
+class Game {
+  constructor() {
+    this.active = false
+    this.backdrop = document.querySelector('.backdrop')
+    this.startBtn = document.querySelector('.start button')
+    this.disc = document.querySelector('.disc')
+    this.player = document.querySelector('audio')
+    this.startBtn.addEventListener('click', () => this.start())
+    this.setBoard()
+  }
+
+  setBoard() {
+    this.board = document.querySelector('.board')
+    this.board.addEventListener('click', event => this.checkColor(event.target))
+    this.bricks = []
+    for (let i = 0; i < 25; i++) {
+      const brick = new Brick()
+      this.bricks.push(brick)
+      this.board.append(brick.element)
+    }
+  }
+
+  toggleActive() {
+    console.log('toggleActive')
+    this.backdrop.style.backgroundImage = ''
+    this.bricks.forEach(brick => brick.toggleActive())
+    this.backdrop.classList.toggle('active')
+    this.backdrop.classList.toggle('start', !this.active)
+    this.active = !this.active
+    this.disc.classList.toggle('spin', this.active)
+    if (this.active) setTimeout(() => this.chooseColor(), rand(4, 12) * 1000)
+  }
+
+  chooseColor() {
+    console.log('chooseColor')
+    this.toggleActive()
+    const brick = pick(this.bricks)
+    this.color = brick.element.style.backgroundColor
+    this.disc.style.color = this.color
+    this.disc.classList.remove('spin')
+    setTimeout(() => this.player.pause(), 200)
+    this.timer = setTimeout(() => this.checkColor(), 2000)
+  }
+
+  checkColor(element) {
+    if (this.active) return
+    clearTimeout(this.timer)
+    const win = (element && element.className === 'brick' && element.style.backgroundColor === this.color)
+    this.backdrop.style.backgroundImage = `url("https://i.gifer.com/${pick(win ? WIN_GIFS : LOOSE_GIFS)}.gif")`
+    this.startBtn.textContent = win ? 'start' : 'try again'
+    setTimeout(() => this.backdrop.classList.add('start'), 3000)
+  }
+
+  start() {
+    this.player.play()
+    this.toggleActive()
+  }
+}
+
+window.addEventListener('load', () => new Game())
